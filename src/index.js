389880ab97af0f199a6e9ff3d2ea526b56af1ad7
Tarun Jain <inmailtarun@gmail.com>
2021-05-11 10:16:15 +0530

    rel no referrer added

--- src/index.js
+++ src/index.js
@@ -95,7 +95,7 @@ const myfirstelement = (
       </div>
     </div>
     <div className="nine col-sm-12">
-      <a href="https://www.facebook.com/groups/1570589976551608" target="_blank"><div className="review-button">
+      <a href="https://www.facebook.com/groups/1570589976551608" target="_blank" rel="noreferrer"><div className="review-button">
       See All Reviews &gt;&gt;
       </div></a>
     </div>
