505c971fbd8efce74f1a48a985da601648ee075b
Tarun Jain <inmailtarun@gmail.com>
2021-05-12 16:45:20 +0530

    Add Hyperlink to listings page and add rel="noreferrer"

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const myfirstelement = (
   <div className="one col-sm-12">
       <div className="contact col-sm-10">
         <div className="phone-container"><i className="fas fa-phone"></i> <span className="number">+91-9870967898</span></div>
-        <div className="email-container"><i className="fas fa-envelope"></i> <span className="email">support@gofresher.org</span></div>
+        <div className="email-container"><i className="fas fa-envelope"></i> <span className="email">support@gofresher.in</span></div>
       </div>
       
       <div className="social-connect col-sm-2">
@@ -69,7 +69,7 @@ const myfirstelement = (
     <div className="client-head col-sm-12">Services</div>
     <div className="dot-and-bar"><span className="line"></span><span className="circle"></span><span className="line"></span></div>
     <div className="client-name col-sm-12">
-      <div className="pri col-sm-4"><div className="client 1"><img src='max.svg' width='150' height='150' alt='Max Fashion'/></div><div className="client 2"><img src='reliance.svg' width='150' height='150' alt='Reliance'/></div></div>
+      <div className="pri col-sm-4"><a href="listings.html" target="_blank" rel="noreferrer"><div className="client 1"><img src='max.svg' width='150' height='150' alt='Max Fashion'/></div></a><div className="client 2"><img src='reliance.svg' width='150' height='150' alt='Reliance'/></div></div>
       <div className="sec col-sm-4"><div className="client 3"><img src='soch.svg' width='150' height='150' alt='Soch Apparels'/></div><div className="client 4"><img src='pantaloons.svg' width='150' height='150' alt='Pantaloons'/></div></div>
       <div className="tre col-sm-4"><div className="client 5"><img src='vishal.svg' width='150' height='150' alt='Vishal Mega Mart'/></div><div className="client 6"><img src='home-shop-18.svg' width='150' height='150' alt='Home Shop 18'/></div></div>
       <div className="fou col-sm-4"><div className="client 7"><img src='more.svg' width='150' height='150' alt='More Retail Office'/></div><div className="client 8"><img src='empty.svg' width='150' height='150' alt='empty'/></div></div>
