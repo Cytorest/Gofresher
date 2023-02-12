7f8b0c976b4a1292ab415b1b9a88d70351ea3a0e
Tarun Jain <inmailtarun@gmail.com>
2023-02-12 18:23:40 +0530

    Schema

--- src/index.js
+++ src/index.js
@@ -4,45 +4,13 @@ import './index.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import reportWebVitals from './reportWebVitals';
 import $ from 'jquery';
+import YourComponent from './schema.js';
 
-const schemaComp = (
+const schema =(
     <div>
-      <script type="application/ld+json">
-        {`
-        {
-          "@context":"https://schema.org",
-          "@type":"WebSite",
-          "url":"https://gofresher-e433c.web.app/",
-          "potentialAction":{
-          "@type":"SearchAction",
-          "target":"https://gofresher-e433c.web.app/search?q={search_term_string}",
-          "query-input":"required name=search_term_string"
-        }
-        `}
-      </script>
-      <script type="application/ld+json">
-        {`
-        {
-          "@context":"http://schema.org/",
-          "@type":"WebPage",
-          "name":"Gofresher - Helping Jobseekers In India",
-          "description":"Gofresher Is A Community Of JobSeekers Seeking Jobs In India. We Help Job Seekers Get A Job Through Our Help.",
-          "keywords":"Gofresher, Gofresher.org, Jobs, Jobs In India, Job Updates In India, Job, Jobseeker",
-          "speakable":{
-          "@type":"SpeakableSpecification",
-          "cssSelector":[
-          "Gofresher - Helping Jobseekers In India",
-          "Gofresher Is A Community Of JobSeekers Seeking Jobs In India. We Help Job Seekers Get A Job Through Our Help."
-          ]
-          },
-          "url":"https://gofresher-e433c.web.app/",
-          "mainContentOfPage":"Online Job Portal",
-          "inLanguage":"en"
-        }
-        `}
-      </script>
+      <YourComponent/>
     </div>
-  )
+  );
 
 const myfirstelement = (
   <div>
@@ -215,7 +183,7 @@ const myfirstelement = (
   
 ReactDOM.render(myfirstelement, document.getElementById('root'));
 
-ReactDOM.render(schemaComp, document.getElementById('schemaComp'));
+ReactDOM.render(schema, document.getElementById('schema');
 
 reportWebVitals();
 
