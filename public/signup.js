fefa480f5e611605aa3edabe5f36dbeed5e31fa9
Tarun Jain <inmailtarun@gmail.com>
2023-02-14 12:39:07 +0530

    Google Sign In On Form

--- public/signup.js
+++ public/signup.js
@@ -1,18 +1,29 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyBHD-tpwQmuzukK-Iu7r96Vzxp-eEXK1Ss",
-    authDomain: "gofresher-e433c.firebaseapp.com",
-    databaseURL: "https://gofresher-e433c-default-rtdb.firebaseio.com",
-    projectId: "gofresher-e433c",
-    storageBucket: "gofresher-e433c.appspot.com",
-    messagingSenderId: "101545999274",
-    appId: "1:101545999274:web:d7d043a00de94c33f5c9d3",
-    measurementId: "G-5EP5C2QDES"
-  };
+    var firebaseConfig = {
+        apiKey: "AIzaSyBHD-tpwQmuzukK-Iu7r96Vzxp-eEXK1Ss",
+        authDomain: "gofresher-e433c.firebaseapp.com",
+        databaseURL: "https://gofresher-e433c-default-rtdb.firebaseio.com",
+        projectId: "gofresher-e433c",
+        storageBucket: "gofresher-e433c.appspot.com",
+        messagingSenderId: "101545999274",
+        appId: "1:101545999274:web:d7d043a00de94c33f5c9d3",
+        measurementId: "G-5EP5C2QDES"
+    };
   
     firebase.initializeApp(firebaseConfig);
     firebase.analytics();
 
     const auth = firebase.auth();
+
+    const signInWithGoogle = () => {
+        const googleProvider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(googleProvider)
+        .then(() =>{
+            window.location.href = "/signup.html";
+        })
+        .catch(error => {
+            console.error(error);
+        })
+    }
 
     // auth.signOut(); | For Testing
 
@@ -158,3 +169,5 @@ var firebaseConfig = {
         $(".two").show();
         $("#message-text").html("Enter Email To Reset Password");
     });
+
+document.getElementById('signInWithGoogle').addEventListener('click', signInWithGoogle);
