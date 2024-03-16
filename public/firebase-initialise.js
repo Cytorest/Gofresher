c8b394575b61b2a6cfb594f41e16a5bce5e1414e
Cytorest <inmailtarun@gmail.com>
2024-03-16 20:00:24 +0530

    Push All Changes

---
+++ public/firebase-initialise.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBHD-tpwQmuzukK-Iu7r96Vzxp-eEXK1Ss",
+    authDomain: "gofresher-e433c.firebaseapp.com",
+    databaseURL: "https://gofresher-e433c-default-rtdb.firebaseio.com",
+    projectId: "gofresher-e433c",
+    storageBucket: "gofresher-e433c.appspot.com",
+    messagingSenderId: "101545999274",
+    appId: "1:101545999274:web:d7d043a00de94c33f5c9d3",
+    measurementId: "G-5EP5C2QDES"
+  };
+  
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
